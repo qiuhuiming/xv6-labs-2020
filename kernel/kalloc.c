9d454fd060ca1304acc60cd6853c9116d00bc7a8
qiuhuiming <1985849912@qq.com>
2021-05-17 13:10:12 +0800

    add: allocate and free mem according to cpuid

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,8 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define LOCKS_NAME_LENGTH ((int)10)
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -19,14 +21,23 @@ struct run {
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
+  struct spinlock locks[NCPU];
+  struct run *freelists[NCPU];
 } kmem;
+
+static char locks_names[NCPU][LOCKS_NAME_LENGTH];
+
+static void kfree_cpuid(void *pa, uint cpu_id);
+
+static void* kalloc_cpuid(uint cpu_id);
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++) {
+    snprintf(locks_names[i], LOCKS_NAME_LENGTH, "kmem_%d", i);
+    initlock(&kmem.locks[i], locks_names[i]);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -34,9 +45,12 @@ void
 freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+  char *start;
+  p = start = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    int cpu_id = (p - start) / PGSIZE % NCPU;
+    kfree_cpuid(p, cpu_id);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -46,37 +60,44 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
-
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
-
-  r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  kfree_cpuid(pa, cpuid());
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
 void *
-kalloc(void)
-{
+kalloc(void) {
+  return kalloc_cpuid(cpuid());
+}
+
+static void kfree_cpuid(void *pa, uint cpu_id) {
+  struct run *r;
+  
+  memset(pa, 1, PGSIZE);
+  r = (struct run*)pa;
+  acquire(&kmem.locks[cpu_id]);
+  r->next = kmem.freelists[cpu_id];
+  kmem.freelists[cpu_id] = r;
+  release(&kmem.locks[cpu_id]);
+}
+
+static void* kalloc_cpuid(uint cpu_id) {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  acquire(&kmem.locks[cpu_id]);
+  r = kmem.freelists[cpu_id];
+  if (r) {
+    kmem.freelists[cpu_id] = r->next;
+  }
+  release(&kmem.locks[cpu_id]);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  if (r) {
+    memset((char*) r, 5, PGSIZE);
+  }
+  return (void*) r;
 }
